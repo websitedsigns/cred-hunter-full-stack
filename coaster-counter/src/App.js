d3e161dafcc1ebd254e9520334ecaf844601b8b9
websitedsigns <websitedsigns@gmail.com>
2023-08-02 14:07:50 +0100

    linked frontend to backend
    Using Mongo

--- coaster-counter/src/App.js
+++ coaster-counter/src/App.js
@@ -1,67 +1,90 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'; // Add this line to import axios
 import CoasterForm from './components/CoasterForm';
 import CoasterTable from './components/CoasterTable';
-import axios from 'axios';
-import './styles/styles.css'; // Import the CSS file
-import './App.css'; // Import the CSS file for background image
+import './styles/styles.css'
+import './App.css'
 
 const App = () => {
   const [coasters, setCoasters] = useState([]);
 
-  // Fetch coaster data from the backend API and update state
-  const fetchCoasters = async () => {
-    try {
-      const response = await axios.get('/api/coasters');
-      setCoasters(response.data);
-    } catch (error) {
-      console.error('Error fetching coasters:', error);
-      // Handle any error that occurred during the request
-    }
-  };
-
+  // Load coaster data from the backend on component mount
   useEffect(() => {
     fetchCoasters();
   }, []);
 
-  const handleAddCoaster = (newCoaster) => {
+  // Fetch coaster data from the backend
+  const fetchCoasters = async () => {
+    try {
+      const response = await fetch('/api/coasters');
+      const data = await response.json();
+      setCoasters(data);
+    } catch (error) {
+      console.error('Error fetching coasters:', error);
+    }
+  };
+
+  const handleAddCoaster = async (newCoaster) => {
     const exists = coasters.some(
       (coaster) =>
         coaster.name === newCoaster.name &&
         coaster.manufacturer === newCoaster.manufacturer &&
         coaster.themePark === newCoaster.themePark
     );
-
+  
     if (!exists) {
       // Add the new coaster with a generated _id property
-      setCoasters([...coasters, { ...newCoaster, _id: Date.now().toString() }]);
-    } else {
-      // Show a confirmation dialog before adding a duplicate coaster
-      if (window.confirm('This coaster already exists in the list. Do you want to add it again?')) {
-        setCoasters([...coasters, { ...newCoaster, _id: Date.now().toString() }]);
+      const updatedCoasters = [...coasters, { ...newCoaster, _id: Date.now().toString() }];
+      setCoasters(updatedCoasters); // Update the state immediately
+      try {
+        const response = await axios.post('/api/coasters', newCoaster);
+        if (response.status === 201) {
+          // Coaster added successfully
+        } else {
+          // Handle error if coaster was not added to the database
+          // You may also want to handle any potential error here
+        }
+      } catch (error) {
+        console.error('Error adding coaster:', error);
+        // Handle any error that occurred during the request
+        // You may also want to update the state back to its previous value if there's an error
       }
+    } else {
+      // Display a warning to the user that the coaster already exists
+      alert('Coaster already exists!');
     }
   };
 
-
-
-  const handleRemoveCoaster = (coasterId) => {
-    setCoasters(coasters.filter((coaster) => coaster._id !== coasterId));
-  };
+  const handleRemoveCoaster = async (coasterId) => {
+    try {
+      console.log('Removing coaster:', coasterId);
   
+      const response = await fetch(`/api/coasters/${coasterId}`, {
+        method: 'DELETE',
+      });
+  
+      console.log('Response:', response);
+  
+      if (response.status === 200) {
+        console.log('Coaster removed successfully:', coasterId);
+        setCoasters(coasters.filter((coaster) => coaster._id !== coasterId));
+      } else {
+        console.log('Failed to remove coaster:', coasterId);
+        // Handle error if coaster was not removed
+      }
+    } catch (error) {
+      console.error('Error removing coaster:', error);
+    }
+  };
+ 
 
   return (
     <div>
-      <div className="header">
-        <h1>Coaster Counter</h1>
-        <div className="coaster-counter-badge">
-          <span>Total Coasters: {coasters.length}</span>
-        </div>
-      </div>
+      <h1 className="header">Coaster Counter</h1>
       <CoasterForm onAddCoaster={handleAddCoaster} existingCoasters={coasters} />
       <CoasterTable coasters={coasters} onRemoveCoaster={handleRemoveCoaster} />
     </div>
   );
 };
-
 
 export default App;
