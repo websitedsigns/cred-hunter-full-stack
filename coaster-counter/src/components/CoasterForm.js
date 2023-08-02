93df3a780f2423bdccafaaceeb7d4f8ba7d27f27
websitedsigns <websitedsigns@gmail.com>
2023-08-02 14:41:31 +0100

    quick entry
    by hitting enter can submit information and then highlighted box goes back to the start for next data point

--- coaster-counter/src/components/CoasterForm.js
+++ coaster-counter/src/components/CoasterForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { manufacturerTypes } from './manufacturerTypes';
 import { themeParks } from './themeParks';
 
@@ -6,23 +6,29 @@ const CoasterForm = ({ onAddCoaster }) => {
   const [name, setName] = useState('');
   const [manufacturer, setManufacturer] = useState('');
   const [themePark, setThemePark] = useState('');
-
-  const capitalizeFirstLetter = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  };
+  const nameInputRef = useRef(null);
+  const themeParkInputRef = useRef(null);
+  const [isThemeParkDropdownOpen, setIsThemeParkDropdownOpen] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name && manufacturer && themePark) {
-      const capitalizedCoaster = {
-        name: capitalizeFirstLetter(name),
-        manufacturer: capitalizeFirstLetter(manufacturer),
-        themePark: capitalizeFirstLetter(themePark),
-      };
-      onAddCoaster(capitalizedCoaster);
+      onAddCoaster({ name, manufacturer, themePark });
       setName('');
       setManufacturer('');
       setThemePark('');
+      nameInputRef.current.focus();
+    }
+  };
+
+  const handleThemeParkChange = (selectedThemePark) => {
+    setThemePark(selectedThemePark);
+    setIsThemeParkDropdownOpen(false);
+  };
+
+  const handleThemeParkKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit(e);
     }
   };
 
@@ -33,23 +39,37 @@ const CoasterForm = ({ onAddCoaster }) => {
         placeholder="Coaster Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
+        ref={nameInputRef}
       />
       <select value={manufacturer} onChange={(e) => setManufacturer(e.target.value)}>
         <option value="">Select Manufacturer</option>
         {manufacturerTypes.map((manufacturer) => (
           <option key={manufacturer} value={manufacturer}>
-            {capitalizeFirstLetter(manufacturer)}
+            {manufacturer}
           </option>
         ))}
       </select>
-      <select value={themePark} onChange={(e) => setThemePark(e.target.value)}>
-        <option value="">Select Theme Park</option>
-        {themeParks.map((themePark) => (
-          <option key={themePark} value={themePark}>
-            {capitalizeFirstLetter(themePark)}
-          </option>
-        ))}
-      </select>
+      <div>
+        <input
+          type="text"
+          placeholder="Theme Park"
+          value={themePark}
+          onChange={(e) => setThemePark(e.target.value)}
+          onFocus={() => setIsThemeParkDropdownOpen(true)}
+          onBlur={() => setIsThemeParkDropdownOpen(false)}
+          onKeyPress={handleThemeParkKeyPress}
+          ref={themeParkInputRef}
+        />
+        {isThemeParkDropdownOpen && (
+          <ul className="theme-park-dropdown">
+            {themeParks.map((themePark) => (
+              <li key={themePark} onClick={() => handleThemeParkChange(themePark)}>
+                {themePark}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
       <button type="submit">Add Coaster</button>
     </form>
   );
